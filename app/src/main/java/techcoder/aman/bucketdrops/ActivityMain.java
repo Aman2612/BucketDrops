18f98cb502a3124dc79dcf7c786f5ba4acdbb1a6
Amandeep <aman9.jap@gmail.com>
2016-07-26 02:45:24 +0530

    Updating changes as I proceed.

--- app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
+++ app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
@@ -2,12 +2,16 @@ package techcoder.aman.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
 
+    Toolbar mtToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mtToolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtToolbar);
     }
 }
