6dda32616166332de8ef557234f97ad7fb8afcae
Amandeep <aman9.jap@gmail.com>
2016-07-26 03:17:05 +0530

    Updating changes as I proceed.

--- app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
+++ app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
@@ -3,6 +3,9 @@ package techcoder.aman.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -13,5 +16,14 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mtToolbar= (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mtToolbar);
+        initBackgroundImage();
+    }
+    private void initBackgroundImage()
+    {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
     }
 }
