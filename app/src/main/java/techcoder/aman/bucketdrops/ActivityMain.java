485642e67b990709c0d6021dcab868177becb28b
Amandeep <aman9.jap@gmail.com>
2016-07-28 20:27:18 +0530

    Updating changes as I proceed.

--- app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
+++ app/src/main/java/techcoder/aman/bucketdrops/ActivityMain.java
@@ -1,8 +1,10 @@
 package techcoder.aman.bucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -10,16 +12,36 @@ import com.bumptech.glide.Glide;
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mtToolbar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"Add");
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mtToolbar= (Toolbar) findViewById(R.id.toolbar);
+        mtToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
+
+
         setSupportActionBar(mtToolbar);
         initBackgroundImage();
     }
-    private void initBackgroundImage()
-    {
+
+    private void initBackgroundImage() {
         ImageView background = (ImageView) findViewById(R.id.iv_background);
         Glide.with(this)
                 .load(R.drawable.background)
@@ -28,3 +50,7 @@ public class ActivityMain extends AppCompatActivity {
         //test
     }
 }
+
+
+
+
---
+++ app/src/main/java/techcoder/aman/bucketdrops/DialogAdd.java
@@ -0,0 +1,24 @@
+package techcoder.aman.bucketdrops;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aman on 27/07/16.
+ */
+public class DialogAdd extends DialogFragment {
+    public DialogAdd() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add,container,false);
+
+
+    }
+}
